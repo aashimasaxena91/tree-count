e590b6605509e1a6e7ef3895ee457051042acb87
Aashima Saxena <aashima.saxena@yahoo.com>
2021-04-11 12:34:33 +0530

    Modified the drawing slate

--- index.js
+++ index.js
@@ -1,65 +1,70 @@
-//canvas name : myPics
-const myPics = document.getElementById("myPics");
-const context = myPics.getContext("2d");
+var canvas = document.getElementById('paint');
+var ctx = canvas.getContext('2d');
 
-// When true, moving the mouse draws on the canvas
-let isDrawing = false;
-let x = 0;
-let y = 0;
+var sketch = document.getElementById('sketch');
+var sketch_style = getComputedStyle(sketch);
+//canvas initial dimension
+canvas.width = 500;
+canvas.height = 250;
 
-// event.offsetX, event.offsetY gives the x and y coordinates of the mouse drawn shape
 
-myPics.addEventListener('mousedown', e => {
-  x = e.offsetX;
-  y = e.offsetY;
-  isDrawing = true;
-});
+// drawing code
+//mouse initial coordinates
+var mouse = {
+  x: 0,
+  y: 0
+};
 
-myPics.addEventListener('mousemove', e => {
-  if (isDrawing === true) {
-    console.log(x, y);
-    drawLine(context, x, y, e.offsetX, e.offsetY);
-    x = e.offsetX;
-    y = e.offsetY;
-  }
-});
+/* Mouse Capturing Work */
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
 
-window.addEventListener('mouseup', e => {
-  if (isDrawing === true) {
-    drawLine(context, x, y, e.offsetX, e.offsetY);
-    x = 0;
-    y = 0;
+/* Drawing on Paint App */
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
 
-    isDrawing = false;
-  }
-});
+ctx.strokeStyle = "black";
 
-function drawLine(context, x1, y1, x2, y2) {
-  context.beginPath();
-  context.strokeStyle = 'black';
-  context.lineWidth = 2;
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.stroke();
-  context.closePath();
-}
 
-//uploading image from users computer
+canvas.addEventListener('mousedown', function(e) {
+  ctx.beginPath();
+  ctx.moveTo(mouse.x, mouse.y);
+
+  canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+
+canvas.addEventListener('mouseup', function() {
+  canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+
+var onPaint = function() {
+  ctx.lineTo(mouse.x, mouse.y);
+  console.log(mouse.x, mouse.y); //this will console the x,y coordinates
+  ctx.stroke();
+};
+
+//uploading the image
+
 const reader = new FileReader();
 const img = new Image();
+
 const uploadImage = (e) => {
   reader.onload = () => {
     img.onload = () => {
       // myPics.width = img.width;
       // myPics.height = img.height;
-      myPics.width = img.naturalWidth;
-      myPics.height = img.naturalHeight;
-      context.drawImage(img, 0, 0, myPics.width, myPics.height);
+      canvas.width = img.naturalWidth;
+      canvas.height = img.naturalHeight;
+      // context.drawImage(img, 0, 0);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     };
     img.src = reader.result;
   };
   reader.readAsDataURL(e.target.files[0]);
 };
+
 
 const imageLoader = document.getElementById("uploader");
 imageLoader.addEventListener("change", uploadImage);
